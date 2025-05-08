4069bbdd197a40491effd3c9d0871b034231d772
fluorine1001 <fluorine1001@sasa.hs.kr>
2025-05-08 21:48:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// 자동 스크롤 기능
 let container = document.getElementById('container');
 let speed = 0;
 
@@ -6,9 +5,9 @@ document.addEventListener('mousemove', function(e) {
     let y = e.clientY;
     let height = window.innerHeight;
     if (y < height * 0.2) {
-        speed = -2; // 위로 이동
+        speed = -3; // 위로 더 빠르게
     } else if (y > height * 0.8) {
-        speed = 2; // 아래로 이동
+        speed = 3; // 아래로 더 빠르게
     } else {
         speed = 0;
     }
@@ -20,7 +19,7 @@ function scrollContainer() {
 }
 requestAnimationFrame(scrollContainer);
 
-// 텍스트 박스 클릭 시 오디오 재생
+// 오디오 재생 부분은 그대로 유지
 document.querySelectorAll('.textbox').forEach(box => {
     box.addEventListener('click', function() {
         let audioFile = this.getAttribute('data-audio');
